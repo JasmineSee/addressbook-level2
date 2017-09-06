0ea4a04db208291633aab37282d715e0d41ccdc3
DESKTOP-7SUIEBU\Jasmine <jgreenteaj@gmail.com>
2017-09-06 11:53:12 +0800

    extract a Contact class so that Phone, Address, and Email can inherit from it

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address extends Contact{
 
     // public static final String EXAMPLE = "123, some street";
     public static final String EXAMPLE = " a/123, Clementi Ave 3, #12-34, 231534 ";
@@ -28,6 +28,7 @@ public class Address {
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
+        super(address,isPrivate);
         String trimmedAddress = address.trim();
         this.isPrivate = isPrivate;
         if (!isValidAddress(trimmedAddress)) {
